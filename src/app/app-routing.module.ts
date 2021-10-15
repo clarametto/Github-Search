6aed70042e1f6d74798968302b69bd2371a3d367
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-15 23:16:48 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'home', component: NavbarComponent },
+  // { path: 'home', component: NavbarComponent },
+  // { path: 'home', component: NavbarComponent },
+  // { path: 'home', component: NavbarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
