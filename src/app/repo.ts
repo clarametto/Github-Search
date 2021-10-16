169cb32b0b191d23eec144ab9cae5e30b35f742d
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-16 12:12:32 +0300

    created repo and user model classes

---
+++ src/app/repo.ts
@@ -0,0 +1,8 @@
+export class Repo {
+    constructor(
+        public name:string,
+        public description:string,
+        public created_at:Date,
+        ){
+    }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public login: string,
+        public avatar_url: any,
+        public followers: any,
+        public following: any,
+        public public_repos: any,
+        public location: any
+      ) {}
+
+}
