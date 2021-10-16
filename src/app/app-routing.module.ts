5aa4681aac8f1b386bc7ebd425f269bb88d69bd9
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-16 13:23:31 +0300

    updated navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserComponent } from './components/user/user.component';
-
+import{RepositoriesComponent} from './repositories/repositories.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 const routes: Routes = [
-  { path: 'home', component: UserComponent },
-  // { path: 'home', component: NavbarComponent },
-  // { path: 'home', component: NavbarComponent },
-  // { path: 'home', component: NavbarComponent },
+{ path: 'userprofile', component: UserComponent},
+{ path: 'repositories', component: RepositoriesComponent},
+{ path: '', redirectTo:"/userprofile", pathMatch:"full"},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,13 +12,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { UserComponent } from './components/user/user.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    UserComponent
+    UserComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,8 @@ import { UserComponent } from './components/user/user.component';
     MatToolbarModule,
     MatButtonModule,
     HttpClientModule,
+    FormsModule
+
 
   ],
   providers: [],
