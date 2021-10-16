81a962b81c509bad99429b3df0716ac266325af2
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-10-16 12:35:46 +0300

    updated user components.ts

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/user';
+import { Repo } from 'src/app/repo';
+import { GitService } from 'src/app/git.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  Users!: User;
+  Repos: Repo[] = [];
+  constructor(public userHttpService: GitService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.searchGit('clarametto');
   }
-
+  searchGit(searchTerm: string) {
+    this.userHttpService.searchGits(searchTerm).then(
+      (success) => {
+        this.Users = this.userHttpService.Users;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.userHttpService.searchRepos(searchTerm).then((success) => {
+      this.Repos = this.userHttpService.Repos;
+    });
+  }
 }
